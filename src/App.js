c7c8a3ab1f08eeccda4c216d69995af6c2823573
lamamomoman <redmaniac0510@gmail.com>
2024-02-23 10:25:24 -0500

    fixed minor issues

--- src/App.js
+++ src/App.js
@@ -274,7 +274,6 @@ function Projects({ projectImages, forMobile }) {
         trigger: '.scroll-text',
         endTrigger: '#projects-wrapper',
         end: 'bottom bottom',
-        toggleActions: 'play none reverse none',
         scrub: 2,
       },
       duration: 10
