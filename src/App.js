1cdddb5a9d9272a3231eb4eaacde83f944ad8c70
lamamomoman <redmaniac0510@gmail.com>
2024-03-04 17:46:14 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -586,8 +586,8 @@ function App() {
         </div>
       </section>
       <section className="page" id="final-image-section">
-        <div id="final-image-wrapper">
-          <img alt="NA" data-scroll data-scroll-speed="-2" src="https://images.unsplash.com/photo-1484626753559-5fa3ea273ae8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <div data-scroll data-scroll-speed={-2} id="final-image-wrapper">
+          <img alt="NA" src="https://images.unsplash.com/photo-1484626753559-5fa3ea273ae8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </div>
       </section>
       <Footer />
